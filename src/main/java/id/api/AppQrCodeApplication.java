790e3c755c6ba72afd3ba91986119d6cc0d6cf81
51222019 <Adira@072025>
2025-07-25 13:53:03 +0700

    first commit

---
+++ src/main/java/id/api/AppQrCodeApplication.java
@@ -0,0 +1,12 @@
+package id.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppQrCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppQrCodeApplication.class, args);
+    }
+}
